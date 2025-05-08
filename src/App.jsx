1edd6cb98f5d97c7ee7bc2171d938ace6ff1e87f
Your Name <your-email@example.com>
2025-05-07 23:07:05 -0700

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,71 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
-import Home from './component/home'
-import Game from './component/game'
-import Start from './component/Start'
-import Contact from './component/Contact'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import './App.css'
+// import Home from './component/home'
+// import Game from './component/game'
+// import Start from './component/Start'
+// import Contact from './component/Contact'
 
+
+// function App() {
+//   const router = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <Home />,
+//       children: [
+//         {
+//           index: true,
+//           element: <Start />,
+//         },
+//         {
+//           path: "/game",
+//           element: (
+//               <Game />
+//           ),
+//         },
+//         {
+//           path: "/contact",
+//           element: (
+//               <Contact />
+//           ),
+//         },
+        
+//       ]
+//     },
+//   ]);
+
+//   return (
+
+//       <RouterProvider router={router}> 
+//       <Home />
+//       </RouterProvider>
+  
+//   );
+// }
+
+// export default App;
+
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './component/Home';
+import Game from './component/Game';
+import Start from './component/Start';
+import Contact from './component/Contact';
+import './App.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    children: [
+      { index: true, element: <Start /> },
+      { path: 'game', element: <Game /> },
+      { path: 'contact', element: <Contact /> },
+    ],
+  },
+]);
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-      children: [
-        {
-          index: true,
-          element: <Start />,
-        },
-        {
-          path: "/game",
-          element: (
-              <Game />
-          ),
-        },
-        {
-          path: "/contact",
-          element: (
-              <Contact />
-          ),
-        },
-        
-      ]
-    },
-  ]);
-
-  return (
-
-      <RouterProvider router={router}> 
-      <Home />
-      </RouterProvider>
-  
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
